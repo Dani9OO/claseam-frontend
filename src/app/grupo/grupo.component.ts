f81304a3331d783e62c1666341bd1eaa88a9c8ad
xAlexei <sukesasuk2010@gmail.com>
2020-03-16 08:57:08 -0600

    .

--- src/app/grupo/grupo.component.ts
+++ src/app/grupo/grupo.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { HorarioComponent } from '../horario/horario.component';
 
 @Component({
   selector: 'app-grupo',
   templateUrl: './grupo.component.html',
   styleUrls: ['./grupo.component.css']
 })
-export class GrupoComponent implements OnInit {
+export class GrupoComponent {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
-  ngOnInit() {
+  dialogRef;
+
+  openHorario(): void {
+    this.dialogRef = this.dialog.open(HorarioComponent, {
+      width: '500px'
+    });
+    this.dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(result);
+    });
   }
 
 }
--- src/app/horario/horario.component.ts
+++ src/app/horario/horario.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
+export interface DialogData{
+    Lunes: Array<Number>;
+    Martes: Array<Number>;
+    Miercoles: Array<Number>;
+    Jueves: Array<Number>;
+    Viernes: Array<Number>;
+}
 @Component({
   selector: 'app-horario',
   templateUrl: './horario.component.html',
   styleUrls: ['./horario.component.css']
 })
-export class HorarioComponent implements OnInit {
+export class HorarioComponent {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<HorarioComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 
-  ngOnInit() {
+  onNoClick(): void {
+    this.dialogRef.close();
   }
+
 
 }
